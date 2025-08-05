4a9c8438800be270f22612af01c00227a3206f76
PurpleNight <ysbhatnagar31@gmail.com>
2025-08-05 09:22:47 +0530

    sum

--- Basics/practicecodes/sum.c
+++ Basics/practicecodes/sum.c
@@ -3,18 +3,15 @@
 int main() {
     int num1, num2, sum;
 
-    // Prompt user for input
     printf("Enter first number: ");
     scanf("%d", &num1);
 
     printf("Enter second number: ");
     scanf("%d", &num2);
 
-    // Calculate sum
     sum = num1 + num2;
 
-    // Display result
     printf("The sum is %d\n", sum);
 
-    return 0;
+    return 0;   
 }
