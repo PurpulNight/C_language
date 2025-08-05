f307379fe61946ad1f36b401e567a9f221ac1a19
PurpleNight <ysbhatnagar31@gmail.com>
2025-08-05 09:54:46 +0530

    area

---
+++ Basics/practicecodes/area_square.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int side;
+    printf("Enter the length of a side of the square: ");
+    scanf("%d", &side);
+
+    printf("The area of the square is %d\n", side * side);
+}
