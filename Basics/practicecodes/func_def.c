e7b45c67499c986a7b9d6e127c23c03fa3534900
PurpleNight <ysbhatnagar31@gmail.com>
2025-08-07 18:58:31 +0530

    function defination

--- Basics/practicecodes/func_def.c
+++ Basics/practicecodes/func_def.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-int main(){
-    void hello(){
-        printf("Hello, World!\n");
-    }
+void hello();
+void hello() {
+    printf("Hello World!\n");
+}   
+int main() {
     hello();
+    return 0;
 }
